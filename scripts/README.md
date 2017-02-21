f7d84285457b9355684e03d66d90c65a6922918c
Qiyu Li <0x9357@gmail.com>
2017-02-21 20:41:18 +0800

    add install readme

---
+++ scripts/README.md
@@ -0,0 +1,7 @@
+```shell
+# root
+sudo bash installer.sh
+
+# normal user 
+chmod +x installer2.sh
+. ./installer2.sh
--- scripts/installer2.sh
+++ scripts/installer2.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+
+source ~/.bashrc
 
 if ! wget https://github.com/quark-zju/lrun/releases/download/v1.1.4/lrun_1.1.4_amd64.deb; then
     echo "Failed to download lrun"
     exit 1
 fi  
-dpkg -i lrun_1.1.4_amd64.deb
-gpasswd -a $USER lrun
+sudo dpkg -i lrun_1.1.4_amd64.deb
+sudo gpasswd -a $USER lrun
     
 git clone https://github.com/quark-zju/ljudge
 cd ljudge
